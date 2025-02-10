218c0c3a34a09ca09d99d74b28fb86bd8329f9e4
Darrion Burgess <dargondab9@gmail.com>
2025-02-09 22:03:32 -0800

    adding initial parser commit

---
+++ grammar.js
@@ -0,0 +1,17 @@
+/**
+ * @file parser for the actions file as defined by the specification therein
+ * @author Darrion Burgess <darrionburgess@gmail.com>
+ * @license MIT
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "actions",
+
+  rules: {
+    // TODO: add the actual grammar rules
+    source_file: $ => "hello"
+  }
+});
