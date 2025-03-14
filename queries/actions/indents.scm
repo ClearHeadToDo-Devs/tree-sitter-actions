d47f43f002c781381f5fbe314286e028b7948562
Darrion Burgess <dargondab9@gmail.com>
2025-03-13 18:05:44 -0700

    feat(queries): add initial queries for folding and indentation based on my project work

---
+++ queries/actions/indents.scm
@@ -0,0 +1,14 @@
+ ((root_action) @indent.begin
+   (#set! indent.immediate 1))
+
+ ((child_action) @indent.begin
+   (#set! indent.immediate 1))
+
+ ((grandchild_action) @indent.begin
+   (#set! indent.immediate 1))
+
+ ((great_grandchild_action) @indent.begin
+   (#set! indent.immediate 1))
+
+ ((leaf_action) @indent.begin
+   (#set! indent.immediate 1))
