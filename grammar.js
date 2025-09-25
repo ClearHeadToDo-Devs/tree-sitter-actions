bfbcf2f8296133a0e018fbb4a23aacfb660ba99a
Darrion Burgess <dargondab9@gmail.com>
2025-09-24 22:17:55 -0700

    formatting

--- grammar.js
+++ grammar.js
@@ -13,56 +13,56 @@ module.exports = grammar({
     action_list: $ => repeat($.root_action),
 
     root_action: $ => seq(
-        field("properties",$.core_properties),
-        optional(field("story", seq(field('icon', '*'), field('text',$.safe_text)))),
-        optional(field("children",repeat1($.child_action)))
+      field("properties", $.core_properties),
+      optional(field("story", seq(field('icon', '*'), field('text', $.safe_text)))),
+      optional(field("children", repeat1($.child_action)))
     ),
 
     child_action: $ => seq(
       field("depth", '>'),
-      field("properties",$.core_properties),
-      optional(field("children",repeat1($.grandchild_action)))
+      field("properties", $.core_properties),
+      optional(field("children", repeat1($.grandchild_action)))
     ),
 
     grandchild_action: $ => seq(
       field("depth", '>>'),
-      field("properties",$.core_properties),
-      optional(field("children",repeat1($.great_grandchild_action)))
+      field("properties", $.core_properties),
+      optional(field("children", repeat1($.great_grandchild_action)))
     ),
 
     great_grandchild_action: $ => seq(
       field('depth', '>>>'),
-      field("properties",$.core_properties),
-      optional(field("children",repeat1($.double_great_grandchild_action)))
+      field("properties", $.core_properties),
+      optional(field("children", repeat1($.double_great_grandchild_action)))
     ),
 
     double_great_grandchild_action: $ => seq(
       field('depth', '>>>>'),
-      field("properties",$.core_properties),
-      optional(field("children",repeat1($.leaf_action)))
+      field("properties", $.core_properties),
+      optional(field("children", repeat1($.leaf_action)))
     ),
 
     leaf_action: $ => seq(
       field('depth', '>>>>>'),
-      field("properties",$.core_properties)
+      field("properties", $.core_properties)
     ),
 
     core_properties: $ => seq(
-        field("state",seq(field('opening','['), field('icon', choice(' ', 'x', '-', '=', '_')),field("closing",']'))),
-        field("name",$.safe_text),
-        optional(repeat1(choice(
-        (field("description",seq(field('icon', '$'), field('text',$.safe_text)))),
-        (field("priority",seq(field('icon','!'), field('number', choice('1', '2', '3', '4'))))),
-        (field("context",seq(field('icon', '+'), field('list', repeat1(seq(field('text', $.safe_text, optional(field('separator',','))))))))),
-        (field("do_date_time",$.do_date_or_time)),
+      field("state", seq(field('opening', '['), field('icon', choice(' ', 'x', '-', '=', '_')), field("closing", ']'))),
+      field("name", $.safe_text),
+      optional(repeat1(choice(
+        (field("description", seq(field('icon', '$'), field('text', $.safe_text)))),
+        (field("priority", seq(field('icon', '!'), field('number', choice('1', '2', '3', '4'))))),
+        (field("context", seq(field('icon', '+'), field('list', repeat1(seq(field('text', $.safe_text, optional(field('separator', ','))))))))),
+        (field("do_date_time", $.do_date_or_time)),
         (field("completed_date_time", $.completed_date)),
-        (field("id",$.id)))))
+        (field("id", $.id)))))
     ),
 
 
     safe_text: $ => /[^$!*+@%>#\(]+/,
-    do_date_or_time: $ => seq(field('icon','@'), $.extended_date_and_time, optional($.recurrance)),
-    completed_date: $ => seq(field('icon','%'), $.date_and_time),
+    do_date_or_time: $ => seq(field('icon', '@'), $.extended_date_and_time, optional($.recurrance)),
+    completed_date: $ => seq(field('icon', '%'), $.date_and_time),
 
     recurrance: $ => seq($.recurrance_icon, $.recurrance_structure),
     recurrance_icon: $ => 'R',
@@ -73,24 +73,21 @@ module.exports = grammar({
 
     extended_date_and_time: $ => seq($.date_and_time, optional($.duration)),
     date_and_time: $ => seq($.date, optional($.time)),
-    date: $ => seq($.year,'-',$.month,'-',$.day),
-    year: $ => /[0-9]{4}/,
-    month: $ => /[0-9]{2}/,
-    day: $ => /[0-9]{2}/,
+    date: $ => seq(field('year', /[0-9]{4}/), '-', field("month", /[0-9]{2}/), '-', field('day', /[0-9]{2}/)),
 
-    time: $ => seq(seq($.hour,':',$.minute), optional(choice('am','pm','AM','PM'))),
+    time: $ => seq(seq($.hour, ':', $.minute), optional(choice('am', 'pm', 'AM', 'PM'))),
     hour: $ => /[0-9]{2}/,
     minute: $ => /[0-9]{2}/,
 
-    duration: $ => seq($.duration_designator, $.duration_value ),
+    duration: $ => seq($.duration_designator, $.duration_value),
     duration_designator: $ => 'D',
     duration_value: $ => /[0-9]+/,
 
     //expects uuid v7
     id: $ => seq($.id_icon, $.uuid),
     id_icon: $ => '#',
-    uuid: $ => seq($.uuid_time_high, optional('-'), $.uuid_time_mid, optional('-'), $.uuid_version_random, optional('-'),$.uuid_variant_random, optional('-'),$.uuid_random),
-    uuid_time_high : $ => /[0-9a-f]{8}/,
+    uuid: $ => seq($.uuid_time_high, optional('-'), $.uuid_time_mid, optional('-'), $.uuid_version_random, optional('-'), $.uuid_variant_random, optional('-'), $.uuid_random),
+    uuid_time_high: $ => /[0-9a-f]{8}/,
     uuid_time_mid: $ => $._uuid_chunk,
     uuid_version_random: $ => $._uuid_chunk,
     uuid_variant_random: $ => $._uuid_chunk,
